0cd38a25ca5983f2f86a8394d9b9fb4ce5fddf2d
Ryan Kienstra <kienstraryan@gmail.com>
2014-11-04 22:24:10 -0800

    removed spaces in favor of tabs

--- adapter-responsive-video.php
+++ adapter-responsive-video.php
@@ -12,32 +12,31 @@ License: GPLv2
 
 add_action( 'init' , 'arv_localization_callback' );
 function arv_localization_callback() {
-	load_plugin_textdomain( 'adapter-responsive-video' , false , basename( dirname( __FILE__ ) ) . '/languages' ); 
+	load_plugin_textdomain( 'adapter-responsive-video' , false , basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 add_action( 'widgets_init' , 'arv_register_widget' );
 function arv_register_widget() {
-	register_widget( 'Adapter_Responsive_Video' ); 
+	register_widget( 'Adapter_Responsive_Video' );
 }
 
 class Adapter_Responsive_Video extends WP_Widget {
 
 	function __construct() {
-		$options = array( 'classname' => 'adapter-responsive-video' ,
-										'description' => __( 'Video from YouTube, Vimeo, and more.' , 'adapter-responsive-video' ) ,
-			 );
-		$this->WP_Widget( 'adapter_responsive_video' , __( 'Adapter Video' , 'adapter-responsive-video' ) , $options );
+		 $options = array( 'classname' => 'adapter-responsive-video' ,
+				'description' => __( 'Video from YouTube, Vimeo, and more.' , 'adapter-responsive-video' ) ,
+		 );
+		 $this->WP_Widget( 'adapter_responsive_video' , __( 'Adapter Video' , 'adapter-responsive-video' ) , $options );
 	}
 
-	function form( $instance )	{
-	
+	function form( $instance ) {
 		$video_url = isset( $instance[ 'video_url' ] ) ? $instance[ 'video_url' ] : "";
 		?>
 			<p>
 				<label for="<?php echo $this->get_field_id( 'video_url' ); ?>">
-	 <?php _e( 'Video url' , 'adapter-responsive-video' ); ?>
-	</label>
-	<input type="text" id="<?php echo $this->get_field_id( 'video_url' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'video_url' ); ?>" value="<?php echo $video_url; ?>" placeholder="e.g. www.youtube.com/watch?v=mOXRZ0eYSA0" \>
+	 			       <?php _e( 'Video url' , 'adapter-responsive-video' ); ?>
+				</label>
+				<input type="text" id="<?php echo $this->get_field_id( 'video_url' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'video_url' ); ?>" value="<?php echo $video_url; ?>" placeholder="e.g. www.youtube.com/watch?v=mOXRZ0eYSA0" \>
 			</p>
 		<?php
 	}
@@ -54,10 +53,10 @@ class Adapter_Responsive_Video extends WP_Widget {
 		return $instance;
 	}
 
-	function widget( $args , $instance ) { 
-		extract( $args );
-		$video_source = isset( $instance[ 'video_source' ] ) ? $instance[ 'video_source' ] : "";		
-		$aspect_ratio_class = isset( $instance[ 'aspect_ratio_class' ] ) ? $instance[ 'aspect_ratio_class' ] : ""; 
+	function widget( $args , $instance ) {
+		extract( $args ); // @todo: remove this
+		$video_source = isset( $instance[ 'video_source' ] ) ? $instance[ 'video_source' ] : "";
+		$aspect_ratio_class = isset( $instance[ 'aspect_ratio_class' ] ) ? $instance[ 'aspect_ratio_class' ] : "";
 		if ( $video_source ) {
 			$bootstrap_responsive_video = get_bootstrap_responsive_video( $video_source , $aspect_ratio_class );
 			echo $before_widget . $bootstrap_responsive_video . $after_widget;
@@ -67,20 +66,20 @@ class Adapter_Responsive_Video extends WP_Widget {
 }
 
 function arv_get_raw_iframe_code( $url ) {
-	$url_no_tags = strip_tags( $url ); 
+	$url_no_tags = strip_tags( $url );
 	$raw_code = wp_oembed_get( $url_no_tags );
 	return $raw_code;
 }
-	
+
 function get_bootstrap_responsive_video( $src , $class ) {
 	$max_width = apply_filters( 'arv_video_max_width' , '580' );
-	return 
+	return
 		"<div class='responsive-video-container' style='max-width:{$max_width}px'>
 			<div class='embed-responsive {$class}'>
 			     <iframe class='embed-responsive-item' src='{$src}'>
 			     </iframe>
 			 </div>
-		 </div>\n"; 
+		 </div>\n";
 }
 
 function arv_get_iframe_attribute( $iframe , $attribute ) {
@@ -91,7 +90,7 @@ function arv_get_iframe_attribute( $iframe , $attribute ) {
 	}
 }
 
-function arv_get_class_for_aspect_ratio( $embed_code ) {		
+function arv_get_class_for_aspect_ratio( $embed_code ) {
 	$bootstrap_apect_ratio = get_bootstrap_aspect_ratio( $embed_code );
 	return	'embed-responsive-' . $bootstrap_apect_ratio;
 }
@@ -107,7 +106,7 @@ function get_bootstrap_aspect_ratio( $embed_code ) {
 function arv_get_raw_aspect_ratio( $embed_code ) {
 	$embed_width = arv_get_iframe_attribute( $embed_code , 'width' );
 	$embed_height =	arv_get_iframe_attribute( $embed_code , 'height' );
-	if ( $embed_width && $embed_height ) { 
+	if ( $embed_width && $embed_height ) {
 		$aspect_ratio = floatval( $embed_width ) / floatval( $embed_height );
 		return $aspect_ratio;
 	}
@@ -116,25 +115,25 @@ function arv_get_raw_aspect_ratio( $embed_code ) {
 function is_ratio_closer_to_four_by_three( $ratio ) {
 	$difference_from_four_by_three = get_difference_from_four_by_three( $ratio );
 	$difference_from_sixteen_by_nine = get_difference_from_sixteen_by_nine( $ratio );
-	return ( $difference_from_four_by_three < $difference_from_sixteen_by_nine ); 
+	return ( $difference_from_four_by_three < $difference_from_sixteen_by_nine );
 }
 
 function get_difference_from_four_by_three( $value ) {
 	$four_by_three = 1.3333;
 	$difference_from_four_by_three = abs( $value - $four_by_three );
-	return $difference_from_four_by_three; 
+	return $difference_from_four_by_three;
 }
 
 function get_difference_from_sixteen_by_nine( $value ) {
 	$sixteen_by_nine = 1.777;
 	$difference_from_sixteen_by_nine = abs( $value - $sixteen_by_nine );
-	return $difference_from_sixteen_by_nine;	
+	return $difference_from_sixteen_by_nine;
 }
 
-add_filter( 'embed_oembed_html' , 'arv_result_filter' ); 
+add_filter( 'embed_oembed_html' , 'arv_result_filter' );
 function arv_result_filter( $raw_embed_code ) {
 	$src = arv_get_iframe_attribute( $raw_embed_code , 'src' );
 	$class = arv_get_class_for_aspect_ratio( $raw_embed_code );
 	$bootstrap_markup = get_bootstrap_responsive_video( $src , $class );
-	return "<div class='post-responsive-video'>" . $bootstrap_markup . "</div>"; 
+	return "<div class='post-responsive-video'>" . $bootstrap_markup . "</div>";
 }
