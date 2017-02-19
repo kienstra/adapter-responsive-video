62e64454855f83765b1aea4f1883d1ee65366b48
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-02-19 09:18:07 -0300

    Correct escaping, including esc_attr.
    Also, substitute single quotes for double quotes.
    And remove space inside brackets.

--- adapter-responsive-video.php
+++ adapter-responsive-video.php
@@ -30,11 +30,11 @@ class Adapter_Responsive_Video extends WP_Widget {
 	}
 
 	function form( $instance ) {
-		$video_url = isset( $instance[ 'video_url' ] ) ? $instance[ 'video_url' ] : "";
+		$video_url = isset( $instance['video_url'] ) ? $instance['video_url'] : '';
 		?>
 			<p>
-				<label for="<?php echo $this->get_field_id( 'video_url' ); ?>">
-	 			       <?php _e( 'Video url' , 'adapter-responsive-video' ); ?>
+				<label for="<?php echo esc_attr( $this->get_field_id( 'video_url' ) ); ?>">
+					<?php esc_html_e( 'Video url' , 'adapter-responsive-video' ); ?>
 				</label>
 				<input type="text" value="<?php echo esc_url( $video_url ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'video_url' ) ); ?>" class="widefat" name="<?php echo esc_attr( $this->get_field_name( 'video_url' ) ); ?>" placeholder="e.g. www.youtube.com/watch?v=mOXRZ0eYSA0" \>
 			</p>
@@ -43,12 +43,12 @@ class Adapter_Responsive_Video extends WP_Widget {
 
 	function update( $new_instance , $previous_instance ) {
 		$instance = $previous_instance;
-		$video_url = isset( $new_instance[ 'video_url' ] ) ? $new_instance[ 'video_url' ] : "";
+		$video_url = isset( $new_instance['video_url'] ) ? $new_instance['video_url'] : '';
 		if ( $video_url ) {
 			$raw_iframe_code = arv_get_raw_iframe_code( $video_url );
-			$instance[ 'video_source' ] = arv_get_iframe_attribute( $raw_iframe_code , 'src' );
-			$instance[ 'aspect_ratio_class' ] = arv_get_class_for_aspect_ratio( $raw_iframe_code );
-			$instance[ 'video_url' ] = strip_tags( $video_url );
+			$instance['video_source'] = arv_get_iframe_attribute( $raw_iframe_code , 'src' );
+			$instance['aspect_ratio_class'] = arv_get_class_for_aspect_ratio( $raw_iframe_code );
+			$instance['video_url'] = esc_url( $video_url );
 		}
 		return $instance;
 	}
@@ -74,8 +74,8 @@ function get_bootstrap_responsive_video( $src , $class ) {
 	return
 		"<div class='responsive-video-container' style='max-width:{$max_width}px'>
 			<div class='embed-responsive {$class}'>
-			     <iframe class='embed-responsive-item' src='{$src}'>
-			     </iframe>
+				 <iframe class='embed-responsive-item' src='{$src}'>
+				 </iframe>
 			 </div>
 		 </div>\n";
 }
