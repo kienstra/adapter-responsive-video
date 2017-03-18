318c602447f548355fe5cc1ed1d785b5def083e4
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-03-18 19:42:37 -0300

    Add more docblocks to the main plugin class, to conform to PHPCS.

--- php/class-adapter-responsive-video-plugin.php
+++ php/class-adapter-responsive-video-plugin.php
@@ -14,16 +14,29 @@ namespace AdapterResponsiveVideo;
  */
 class Adapter_Responsive_Video_Plugin {
 
+	/**
+	 * Construct the class.
+	 */
 	public function __construct() {
 		require_once dirname( __FILE__ ) . '/class-adapter-responsive-video.php';
 		add_action( 'init' , array( $this, 'plugin_localization' ) );
 		add_action( 'widgets_init' , array( $this, 'register_widget' ) );
 	}
 
+	/**
+	 * Load the textdomain for the plugin, enabling translation.
+	 *
+	 * @return void.
+	 */
 	public function plugin_localization() {
 		load_plugin_textdomain( 'adapter-responsive-video' , false , basename( dirname( __FILE__ ) ) . '/languages' );
 	}
 
+	/**
+	 * Register the Adapter Responsive Video widget.
+	 *
+	 * @return void.
+	 */
 	public function register_widget() {
 		register_widget( 'AdapterResponsiveVideo\Adapter_Responsive_Video' );
 	}
